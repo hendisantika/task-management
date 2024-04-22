aa4393e6546ea04208254b136df4a6727597c097
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:56:10 +0700

    Add getQueryParamValues Utils 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/taskmanagement/utils/requests/RequestHandler.kt
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/utils/requests/RequestHandler.kt
@@ -1,5 +1,6 @@
 package id.my.hendisantika.taskmanagement.utils.requests
 
+import org.springframework.util.MultiValueMap
 import org.springframework.web.reactive.function.server.ServerRequest
 import kotlin.jvm.optionals.getOrNull
 
@@ -20,5 +21,11 @@ private typealias QueryParam = String
 fun getQueryParam(request: ServerRequest): (QueryParam) -> String? {
     return { param ->
         request.queryParam(param).getOrNull()
+    }
+}
+
+fun getQueryParamValues(queryParams: MultiValueMap<String, String>): (QueryParam) -> List<String> {
+    return { param ->
+        queryParams[param].orEmpty()
     }
 }
