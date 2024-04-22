ec76386395a43e4318b0810fd3390834dad8105c
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:37:29 +0700

    Add tasks schemas.sql 🫸🌀✏️📗🐧🐳

--- src/main/resources/schemas.sql
+++ src/main/resources/schemas.sql
@@ -25,3 +25,46 @@ CREATE TABLE if NOT EXISTS users
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     version INT NOT NULL
     );
+
+-- CREATE TYPE Status AS ENUM('PENDING', 'IN_PROGRESS', 'COMPLETED');
+CREATE TABLE if NOT EXISTS tasks
+(
+    id
+    SERIAL
+    PRIMARY
+    KEY,
+    title
+    VARCHAR
+(
+    75
+) NOT NULL,
+    description VARCHAR
+(
+    255
+),
+    due_date DATE NOT NULL,
+    status VARCHAR
+(
+    20
+) CHECK
+(
+    status
+    IN
+(
+    'PENDING',
+    'IN_PROGRESS',
+    'COMPLETED'
+)) NOT NULL,
+    created_by INT REFERENCES users
+(
+    id
+),
+    updated_by INT REFERENCES users
+(
+    id
+),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   user_id INT REFERENCES users(id),
+    version INT NOT NULL
+    );
