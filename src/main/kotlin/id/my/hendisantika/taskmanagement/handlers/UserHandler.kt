3bdb8cd6ff875b809a097fc91625d0af8b44780b
hendisantika <hendisantika@yahoo.co.id>
2024-04-23 06:55:48 +0700

    Add byId in UserHandler🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/taskmanagement/handlers/UserHandler.kt
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/handlers/UserHandler.kt
@@ -2,14 +2,17 @@ package id.my.hendisantika.taskmanagement.handlers
 
 import id.my.hendisantika.taskmanagement.entities.User
 import id.my.hendisantika.taskmanagement.services.UserService
+import id.my.hendisantika.taskmanagement.utils.requests.getPathId
 import id.my.hendisantika.taskmanagement.utils.responses.responseNotFound
 import jakarta.validation.Validator
 import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitSingle
 import org.apache.logging.log4j.LogManager
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.bodyAndAwait
+import reactor.kotlin.core.publisher.switchIfEmpty
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,13 @@ class UserHandler(private val service: UserService, private val validator: Valid
 
     suspend fun all(request: ServerRequest) =
         ServerResponse.ok().bodyAndAwait(service.all().map(User::toUserResponse).asFlow())
+
+    suspend fun byId(request: ServerRequest): ServerResponse {
+        val id = getPathId(request)
+
+        return service.byId(id)
+            .flatMap { ServerResponse.ok().bodyValue(it.toUserResponse()) }
+            .switchIfEmpty { userResponseNotFound(id) }.awaitSingle()
+    }
 
 }
