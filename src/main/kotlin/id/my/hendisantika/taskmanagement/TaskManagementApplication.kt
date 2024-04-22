0a8029b34554f192d9c19608081efa6cacd434ce
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:34:21 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/TaskManagementApplication.kt
@@ -0,0 +1,11 @@
+package id.my.hendisantika.taskmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<TaskManagementApplication>(*args)
+}
---
+++ src/test/kotlin/id/my/hendisantika/taskmanagement/TaskManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.my.hendisantika.taskmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
