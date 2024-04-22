27842196252b70c5fcd6328cf57a96fd9b5fbde6
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:49:07 +0700

    Add findAllByConditions in TaskRepository 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/taskmanagement/repositories/TaskRepository.kt
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/repositories/TaskRepository.kt
@@ -1,7 +1,12 @@
 package id.my.hendisantika.taskmanagement.repositories
 
 import id.my.hendisantika.taskmanagement.entities.Task
+import id.my.hendisantika.taskmanagement.entities.TaskStatus
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.r2dbc.repository.R2dbcRepository
+import org.springframework.data.repository.query.Param
+import reactor.core.publisher.Flux
+import java.time.LocalDate
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +18,44 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository
  * Time: 10:48
  * To change this template use File | Settings | File Templates.
  */
-interface TaskRepository : R2dbcRepository<Task, Long>
+interface TaskRepository : R2dbcRepository<Task, Long> {
+    @Query(
+        """
+    SELECT * FROM tasks
+          /* :createdBy IS NULL */
+    WHERE (:createdbyisnull OR created_by IN (:createdby))
+          /* :updatedBy IS NULL */
+      AND (:updatedbyisnull OR updated_by IN (:updatedby))
+          /* :dueDate IS NULL */
+      AND (:duedatesisnull OR due_date IN (:duedates))
+          /* :status IS NULL */
+      AND (:statusesisnull OR status IN (COALESCE(:statuses, 'PENDING')))
+    """
+    )
+    fun findAllByConditions(
+        @Param("createdBy") createdBy: List<Long>,
+        @Param("updatedBy") updatedBy: List<Long>,
+        @Param("dueDates") dueDates: List<LocalDate>,
+        @Param("statuses") statuses: List<TaskStatus>,
+        /**```kt
+         * queryParams.createdByUsers.isNullOrEmpty()
+         * ```
+         */
+        createdByIsNull: Boolean,
+        /**```kt
+         * queryParams.updatedByUsers.isNullOrEmpty()
+         * ```
+         */
+        updatedByIsNull: Boolean,
+        /**```kt
+         * queryParams.dueDates.isNullOrEmpty()
+         * ```
+         */
+        dueDatesIsNull: Boolean,
+        /**```kt
+         * queryParams.statuses.isNullOrEmpty()
+         * ```
+         */
+        statusesIsNull: Boolean,
+    ): Flux<Task>
+}
