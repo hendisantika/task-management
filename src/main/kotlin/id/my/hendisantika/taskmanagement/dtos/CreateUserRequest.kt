706935ed9a6d077f308f47e781d397e8c561b293
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:41:51 +0700

    Add UpdateUserRequest DTOs 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/taskmanagement/dtos/CreateUserRequest.kt
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/dtos/CreateUserRequest.kt
@@ -31,3 +31,16 @@ data class CreateUserRequest(
     @field:Size(min = 2, message = "lastName must be [2] characters or more")
     val lastName: String,
 )
+
+data class UpdateUserRequest(
+    @field:Email(message = "email format must be correct (eg. email@example.com)")
+    val email: String,
+
+    @field:NotBlank(message = "firstName must not be empty")
+    @field:Size(min = 2, message = "firstName must be [2] characters or more")
+    val firstName: String,
+
+    @field:NotBlank(message = "lastName must not be empty")
+    @field:Size(min = 2, message = "lastName must be [2] characters or more")
+    val lastName: String,
+)
