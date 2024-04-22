927cc44a589532c8c89d10a4b7aa7644d3bd7732
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:41:39 +0700

    Add CreateUserRequest DTOs 🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/dtos/CreateUserRequest.kt
@@ -0,0 +1,33 @@
+package id.my.hendisantika.taskmanagement.dtos
+
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : task-management
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/22/24
+ * Time: 10:41
+ * To change this template use File | Settings | File Templates.
+ */
+data class CreateUserRequest(
+    @field:Email(message = "email format must be correct (eg. email@example.com)")
+    val email: String,
+
+    // TODO: Hash Password
+    @field:NotBlank(message = "password must not be empty")
+    @field:Size(min = 6, message = "password must be [6] characters or more")
+    val password: String,
+
+    @field:NotBlank(message = "firstName must not be empty")
+    @field:Size(min = 2, message = "firstName must be [2] characters or more")
+    val firstName: String,
+
+    @field:NotBlank(message = "lastName must not be empty")
+    @field:Size(min = 2, message = "lastName must be [2] characters or more")
+    val lastName: String,
+)
