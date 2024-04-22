d91b1bd3f71b13356009c82d3ab3af005a246904
hendisantika <hendisantika@yahoo.co.id>
2024-04-22 10:50:55 +0700

    Add all in TaskHandler 🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/taskmanagement/handlers/TaskHandler.kt
+++ src/main/kotlin/id/my/hendisantika/taskmanagement/handlers/TaskHandler.kt
@@ -1,6 +1,10 @@
 package id.my.hendisantika.taskmanagement.handlers
 
+import id.my.hendisantika.taskmanagement.dtos.TaskQueryParamValues
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyAndAwait
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,5 +24,24 @@ class TaskHandler(
 ) {
     private val log = LogManager.getLogger(TaskHandler::class.java)
     private val taskRespNotFound = responseNotFound(Task::class, TaskHandler::class)
+
+    suspend fun all(request: ServerRequest): ServerResponse {
+        val queryParams = request.queryParams()
+        val getRequestParamValues = getQueryParamValues(queryParams)
+        val getIdsFromQueryParams = getRequestParamValues then ::toLongs
+
+        val taskQueryParams = TaskQueryParamValues(
+            dueDates = (getRequestParamValues then ::toLocalDates)("due-date"),
+            statuses = (getRequestParamValues then ::toStatuses)("status"),
+            createdByUsers = getIdsFromQueryParams("created-by"),
+            updatedByUsers = getIdsFromQueryParams("updated-by"),
+        )
+
+        val fetchedTasks =
+            if (queryParams.isEmpty()) service.all()
+            else service.allByQueryParams(taskQueryParams)
+
+        return ServerResponse.ok().bodyAndAwait(fetchedTasks.map(Task::toTaskResponse).asFlow())
+    }
 
 }
